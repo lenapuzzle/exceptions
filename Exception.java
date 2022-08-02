353ac49c09c5d06b39041a90061409413df24fe1
lenapuzzle <lenapuzzle@yahoo.com>
2022-08-01 22:24:31 -0700

    exceptions_java

---
+++ Exception.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Exception {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+            myList.add("13");
+            myList.add("hello world");
+            myList.add(48);
+            myList.add("Goodbye World");
+
+            try {
+            for(int i = 0; i < myList.size(); i++) {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            }
+            } catch(ClassCastException e) {
+                System.out.println("Error casting into integer");
+                for(int i = 0; i < myList.size(); i++) {
+                    Object castedValue = myList.get(i);
+                    System.out.println(castedValue);
+                }
+            }
+                
+            
+    }
+}
